2c8a80178d4d014f3cb5babc4acae27e141b8054
Ross Gray <rosswgray@gmail.com>
2020-11-24 15:16:06 +0800

    added API routes and WeChat login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,2 +1,25 @@
 class LoginController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
+  URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
+
+  def wechat_user
+    wechat_params = {
+      appId: ENV["WECHAT_APP_ID"],
+      secret: ENV["WECHAT_APP_SECRET"],
+      js_code: params[:code],
+      grant_type: "authorization_code"
+    }
+
+    @wechat_response ||= RestClient.get(URL, params: wechat_params)
+    @wechat_user ||= JSON.parse(@wechat_response.body)
+  end
+
+  def login
+    @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
+    render json: {
+      user: @user
+    }
+  end
+
 end

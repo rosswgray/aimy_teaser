997a32a391c547b4a7416080f1e8e696cc859010
Ross Gray <rosswgray@gmail.com>
2020-11-26 15:22:58 +0800

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_053348) do
+ActiveRecord::Schema.define(version: 2020_11_26_070034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_11_24_053348) do
     t.integer "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "main_photo"
+    t.integer "capacity"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 

275fc882efd361524f73988f24b2b5de48a8cc30
Arnaud Gazielly <arnaudgazielly@yahoo.com>
2020-11-26 13:57:19 +0800

    add Activity Model method and call it in index method in activities controller

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ActivitiesController < Api::V1::BaseController
   def index
+    query = params [:query]
+    If query.present?
+    Activity.global_search(query)
+    # @activities = Activity.joins(:organizer).where("title ILIKE :query, description ILIKE :query, organizers.name ILIKE :query", query: "%#{query}%")
+d.	else
     @activities = Activity.all
     render json: @activities #Just for testing
   end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,13 @@
 class Activity < ApplicationRecord
-  belongs_to :organizer, class_name: "User" 
+  belongs_to :organizer, class_name: "User"
+
+  include PgSearch::Model
+  pg_search scope :global_search,
+  against: [:title, :description],
+  associated_against: {
+    organizer: [:name]
+  }
+  using: {
+      tsearch: { prefix: true }
+    }
 end

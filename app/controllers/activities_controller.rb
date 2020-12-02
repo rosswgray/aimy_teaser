f8516fe49051c7ad44de3083f4c70bda24e05785
Ross Gray <rosswgray@gmail.com>
2020-12-02 14:10:35 +0800

    link to show working

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -11,8 +11,21 @@ class ActivitiesController < ApplicationController
     # end
   end
 
+  def edit; end
+
+  def update
+    @activty.update(activity_params)
+    render :new
+  end
+
   def show
     @activity = Activity.find(params[:id])
     @sessions = @activity.sessions
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :description, :main_photo, :photo_1, :photo_2, :photo_3, :price, :rating, :latitude, :longitude)
   end
 end

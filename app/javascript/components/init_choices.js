3689f6b1f402dd2710948f135f23a216da1187a9
Ross Gray <rosswgray@gmail.com>
2020-12-02 18:19:43 +0800

    added to js components

---
+++ app/javascript/components/init_choices.js
@@ -0,0 +1,15 @@
+const Choices = require('choices.js')
+
+const initChoices = () => {
+  const element = document.getElementById('activity_tag_list');
+  console.log('inside initChoices')
+  if (element) {
+    console.log('inside initChoices, element found!')
+    new Choices(element, {
+      removeItems: true,
+      removeItemButton: true,
+    });
+  }
+}
+
+export { initChoices }

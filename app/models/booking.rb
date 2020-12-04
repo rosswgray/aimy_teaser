666d5c486461e6196a7eba5cfa7d888a03f34856
Ross Gray <rosswgray@gmail.com>
2020-12-04 13:09:52 +0800

    posts for bookings working?

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,18 @@
 class Booking < ApplicationRecord
   belongs_to :session
-  # belongs_to :user
-  belongs_to :parent, class_name: "User", polymorphic: true, foreign_key: :user_id
+  # belongs_to :parent, class_name: "User", foreign_key: :user_id, polymorphic: true
+  belongs_to :user
   has_one :activity, through: :sessions
   has_one :instructor, through: :sessions
+
+  def parent
+    User.find(self.user_id)
+  end
+
+  def parent=(new_parent)
+    self.user = new_parent
+  end
 end
+
+# removed code
+# polymorphic: true,

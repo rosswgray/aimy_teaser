aa3010c478c9307e5a0de7ead772e1136de77c96
Ross Gray <rosswgray@gmail.com>
2020-12-07 14:18:46 +0800

    removes one puts

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -6,7 +6,6 @@ class Api::V1::ActivitiesController < Api::V1::BaseController
       # @activities = Activity.joins(:organizer).where("title ILIKE :query, description ILIKE :query, organizers.name ILIKE :query", query: "%#{query}%")
     else
       @activities = Activity.all
-      # p @activities
     end
     # KEEP LINE 12 commented out
     # render json: @activities #Just for testing
@@ -16,5 +15,4 @@ class Api::V1::ActivitiesController < Api::V1::BaseController
     @activity = Activity.find(params[:id])
     @sessions = @activity.sessions
   end
-
 end

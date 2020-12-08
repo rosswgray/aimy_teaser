161b17814fc04f348e9d2d7d25aa63afaa17498d
Ross Gray <rosswgray@gmail.com>
2020-12-08 17:28:18 +0800

    instructor index

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -13,8 +13,18 @@ class InstructorsController < ApplicationController
   end
 
   def edit
+    @instructor = Instructor.find(params[:id])
   end
 
   def update
+    @instructor = Instructor.find(params[:id])
+    @instructor.update(instructor_params)
+    redirect_to instructors_path
+  end
+
+  private
+
+  def instructor_params
+    params.require(:instructor).permit(:user_id, :name, :years_experience, :date_started, :certifications, :verified, :blurb)
   end
 end

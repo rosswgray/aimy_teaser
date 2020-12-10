d1eca051ccd4465bfe0e23b104c0cf21cbb88105
Ross Gray <rosswgray@gmail.com>
2020-12-10 10:23:23 +0800

    addres migration for requirements and form field on new activity

--- db/migrate/20201210021748_add_requirements_to_activity.rb
+++ db/migrate/20201210021748_add_requirements_to_activity.rb
@@ -1,5 +1,5 @@
 class AddRequirementsToActivity < ActiveRecord::Migration[6.0]
   def change
-    add_column :activities, :requirements, :text
+    add_column :activities, :requirements, :text, array: true, default: ["water bottle", "gym shoes"]
   end
 end
